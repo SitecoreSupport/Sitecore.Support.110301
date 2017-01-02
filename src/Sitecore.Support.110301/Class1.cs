a7e1b76938a67a8909002e9b6cc4a3b3792aa38f
AlexanderYaremenko <ay@sitecore.net>
2017-01-02 15:00:45 +0200

    Commit path for WFFM 2.4.4

---
+++ src/Sitecore.Support.110301/Class1.cs
@@ -0,0 +1,55 @@
+﻿using Sitecore.Form.Core.Pipelines.ProcessMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace Sitecore.Support.Form.Core.Pipelines.ProcessMessage
+{
+    public class ProcessMessage
+    {
+
+        private MailMessage GetMail(ProcessMessageArgs args)
+        {
+            MailMessage mail = new MailMessage(args.From.Replace(";", ","), args.To.Replace(";", ",").ToString(), args.Subject.ToString(), args.Mail.ToString())
+            {
+                IsBodyHtml = args.IsBodyHtml
+            };
+            if (args.CC.Length > 0)
+            {
+                char[] separator = new char[] { ',' };
+                foreach (string str in args.CC.Replace(";", ",").ToString().Split(separator))
+                {
+                    mail.CC.Add(new MailAddress(str));
+                }
+            }
+            if (args.BCC.Length > 0)
+            {
+                char[] chArray2 = new char[] { ',' };
+                foreach (string str2 in args.BCC.Replace(";", ",").ToString().Split(chArray2))
+                {
+                    mail.Bcc.Add(new MailAddress(str2));
+                }
+            }
+            args.Attachments.ForEach(delegate (Attachment attachment) {
+                mail.Attachments.Add(attachment);
+            });
+            return mail;
+        }
+        public void SendEmail(ProcessMessageArgs args)
+        {
+            SmtpClient client = new SmtpClient(args.Host)
+            {
+                EnableSsl = args.EnableSsl
+            };
+            if (args.Port != 0)
+            {
+                client.Port = args.Port;
+            }
+            client.Credentials = args.Credentials;
+            client.Send(this.GetMail(args));
+        }
+
+    }
+}
---
+++ src/Sitecore.Support.110301/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.110301")]
+[assembly: AssemblyProduct("Sitecore.Support.110301")]
+[assembly: ComVisible(false)]
